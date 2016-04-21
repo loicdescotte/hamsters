13a455fa7c1d379d76e41363985aa775cc0a21d6
Ikhoon Eom <ih.pert@gmail.com>
2016-04-22 00:16:31 +0900

    Add monad transformers for com.twitter.util.Future[T]

--- README.md
+++ README.md
@@ -12,16 +12,18 @@ Validation is right biased, i.e. Right is used for the "success side" and left f
  val e1: Either[String, Int] = Right(1)
  val e2: Either[String, Int] = Left("error 1")
  val e3: Either[String, Int] = Left("error 2")
- 
+
  val validation = Validation(e1,e2, e3)
  val failures = validation.failures //List[String] : List("error 1", "error 2")
  val successes = validation.successes //List[Int] : List(1)
 ```
- 
+
 ##  Simple monad transformers
 
-Example : combine Future and Option types then make it work in a for comprehension.  
+Example : combine Future and Option types then make it work in a for comprehension.
 More information on why it's useful [here](http://loicdescotte.github.io/posts/scala-compose-option-future/).
+
+### Scala Future
 
 ```scala
 def foa: Future[Option[String]] = Future(Some("a"))
@@ -32,7 +34,21 @@ val composedAB: Future[Option[String]] = (for {
   ab <- FutureOption(fob(a))
 } yield ab).future
 ```
-Currently hamsters only supports FutureEither and FutureOption monad transformers but more will come!
+
+### [Twitter Future](https://github.com/twitter/util#futures)
+```scala
+import com.twitter.util.Future
+
+def foa: Future[Option[String]] = Future(Some("a"))
+def fob(a: String): Future[Option[String]] = Future(Some(a+"b"))
+
+val composedAB: Future[Option[String]] = (for {
+  a <- TwitterFutureOption(foa)
+  ab <- TwitterFutureOption(fob(a))
+} yield ab).future
+```
+
+Currently hamsters only supports FutureEither, FutureOption, TwitterFutureEither and TwitterFutureOption monad transformers but more will come!
 
 ## Union types
 
@@ -49,7 +65,7 @@ def jsonElement(x: Int): Union3[String, Int, Double] = {
 
 ## Right biased Either
 
-Either is not biased is standard Scala library. With this helper, `map` and `flatMap` can be used by default as on the right side of Either, for example in for comprehension. 
+Either is not biased is standard Scala library. With this helper, `map` and `flatMap` can be used by default as on the right side of Either, for example in for comprehension.
 
 ```scala
 import io.github.hamsters.Implicits._
@@ -65,8 +81,8 @@ for {
   v3 <- e3
 } yield(s"$v1-$v2-$v3")  //Left("nan")
 ```
- 
-## Coming soon 
+
+## Coming soon
 
  * Simple HList with conversions from/to tuples
  * More cool stuffs!
--- src/main/scala/io/github/hamsters/MonadTransformers.scala
+++ src/main/scala/io/github/hamsters/MonadTransformers.scala
@@ -1,6 +1,8 @@
 package io.github.hamsters
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.{Failure, Success, Try}
+import com.twitter.{util => twitter}
 
 case class FutureOption[+A](future: Future[Option[A]]) extends AnyVal {
   def flatMap[B](f: A => FutureOption[B])(implicit ec: ExecutionContext): FutureOption[B] = {
@@ -24,6 +26,7 @@ case class FutureOption[+A](future: Future[Option[A]]) extends AnyVal {
     })
   }
 }
+
 
 case class FutureEither[L, +R](future: Future[Either[L, R]]) extends AnyVal {
   def flatMap[R2](f: R => FutureEither[L, R2])(implicit ec: ExecutionContext): FutureEither[L, R2] = {
@@ -52,6 +55,75 @@ case class FutureEither[L, +R](future: Future[Either[L, R]]) extends AnyVal {
       case Right(r) => if (p(r)) Right(r) else Left(default)
       case _ => Left(default)
     })
+  }
+}
+
+import TwitterFutureOps._
+case class TwitterFutureOption[+A](future: twitter.Future[Option[A]]) extends AnyVal {
+  def flatMap[B](f: A => TwitterFutureOption[B])(implicit ec: ExecutionContext): TwitterFutureOption[B] =
+    futureOption.flatMap(f andThen(twitterToScalaFutureOption(_)))
+
+  def map[B](f: A => B)(implicit ec: ExecutionContext): TwitterFutureOption[B] = futureOption.map(f)
+
+  def filter(p: (A) ⇒ Boolean)(implicit ec: ExecutionContext): TwitterFutureOption[A] = filterWith(p)
+
+  def filterWith(p: (A) ⇒ Boolean)(implicit ec: ExecutionContext): TwitterFutureOption[A] = futureOption.filter(p)
+
+  private def futureOption: FutureOption[A] = FutureOption(future)
+}
+
+case class TwitterFutureEither[L, +R](future: twitter.Future[Either[L, R]]) extends AnyVal {
+  def flatMap[R2](f: R => TwitterFutureEither[L, R2])(implicit ec: ExecutionContext): TwitterFutureEither[L, R2] =
+    futureEither.flatMap(f andThen(twitterToScalaFutureEither(_)))
+
+  def map[R2](f: R => R2)(implicit ec: ExecutionContext): TwitterFutureEither[L, R2] = futureEither.map(f)
+
+  def filter(p: (R) ⇒ Boolean)(implicit ec: ExecutionContext): TwitterFutureEither[String, R] = filterWith(p)
+
+  def filterWith(p: (R) ⇒ Boolean)(implicit ec: ExecutionContext): TwitterFutureEither[String, R] = futureEither.filterWith(p)
+
+  def filterWithDefault(p: (R) ⇒ Boolean, default: L)(implicit ec: ExecutionContext): TwitterFutureEither[L, R] = futureEither.filterWithDefault(p, default)
+
+  private def futureEither: FutureEither[L, R] = FutureEither(future)
+}
+
+
+object TwitterFutureOps {
+  implicit def scalaToTwitterTry[T](t: Try[T]): twitter.Try[T] = t match {
+    case Success(r) => twitter.Return(r)
+    case Failure(ex) => twitter.Throw(ex)
+  }
+
+  implicit def twitterToScalaTry[T](t: twitter.Try[T]): Try[T] = t match {
+    case twitter.Return(r) => Success(r)
+    case twitter.Throw(ex) => Failure(ex)
+  }
+
+  implicit def scalaToTwitterFuture[T](f: Future[T])(implicit ec: ExecutionContext): twitter.Future[T] = {
+    val promise = twitter.Promise[T]()
+    f.onComplete(promise update _)
+    promise
+  }
+
+  implicit def twitterToScalaFuture[T](f: twitter.Future[T]): Future[T] = {
+    val promise = Promise[T]()
+    f.respond(promise complete _)
+    promise.future
+  }
+
+  implicit def twitterToScalaFutureOption[T](f: TwitterFutureOption[T])(implicit ec: ExecutionContext) : FutureOption[T] = {
+    FutureOption(f.future)
+  }
+
+  implicit def twitterToScalaFutureEither[L, R](f: TwitterFutureEither[L, R])(implicit ec: ExecutionContext) : FutureEither[L, R] = {
+    FutureEither(f.future)
+  }
+  implicit def scalaToTwitterFutureOption[T](f: FutureOption[T])(implicit ec: ExecutionContext) : TwitterFutureOption[T] = {
+    TwitterFutureOption(f.future)
+  }
+
+  implicit def scalaToTwitterFutureEither[L, R](f: FutureEither[L, R])(implicit ec: ExecutionContext) : TwitterFutureEither[L, R] = {
+    TwitterFutureEither(f.future)
   }
 
 }
